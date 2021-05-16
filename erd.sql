0c3653bb538beace82029ec7dd11170a62002bcf
Alyssa <aebowden36@gmail.com>
2021-05-16 16:14:11 -0400

    added erd image

--- erd.sql
+++ erd.sql
@@ -3,65 +3,65 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "Departments" (
-    "dept_no" VARCHAR   NOT NULL,
-    "dept_name" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Departments" PRIMARY KEY (
-        "dept_no"
+CREATE TABLE Departments (
+    dept_no VARCHAR   NOT NULL,
+    dept_name VARCHAR   NOT NULL,
+    CONSTRAINT pk_Departments PRIMARY KEY (
+        dept_no
      )
 );
 
-CREATE TABLE "Dept_emp" (
-    "emp_no" INTEGER   NOT NULL,
-    "dept_no" VARCHAR   NOT NULL
+CREATE TABLE Dept_emp (
+    emp_no INTEGER   NOT NULL,
+    dept_no VARCHAR   NOT NULL
 );
 
-CREATE TABLE "Dept_manager" (
-    "dept_no" VARCHAR   NOT NULL,
-    "emp_no" INTEGER   NOT NULL
+CREATE TABLE Dept_manager (
+    dept_no VARCHAR   NOT NULL,
+    emp_no INTEGER   NOT NULL
 );
 
-CREATE TABLE "Employee" (
-    "emp_no" INTEGER   NOT NULL,
-    "emp_title" VARCHAR   NOT NULL,
-    "birth_date" DATE   NOT NULL,
-    "first_name" VARCHAR   NOT NULL,
-    "last_name" VARCHAR   NOT NULL,
-    "sex" VARCHAR   NOT NULL,
-    "hire_date" DATE   NOT NULL,
-    CONSTRAINT "pk_Employee" PRIMARY KEY (
-        "emp_no"
+CREATE TABLE Employee (
+    emp_no INTEGER   NOT NULL,
+    emp_title VARCHAR   NOT NULL,
+    birth_date DATE   NOT NULL,
+    first_name VARCHAR   NOT NULL,
+    last_name VARCHAR   NOT NULL,
+    sex VARCHAR   NOT NULL,
+    hire_date DATE   NOT NULL,
+    CONSTRAINT pk_Employee PRIMARY KEY (
+        emp_no
      )
 );
 
-CREATE TABLE "Salaries" (
-    "emp_no" INTEGER   NOT NULL,
-    "salary" INTEGER   NOT NULL
+CREATE TABLE Salaries (
+    emp_no INTEGER   NOT NULL,
+    salary INTEGER   NOT NULL
 );
 
-CREATE TABLE "Title" (
-    "title_id" VARCHAR   NOT NULL,
-    "title" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Title" PRIMARY KEY (
-        "title_id"
+CREATE TABLE Title (
+    title_id VARCHAR   NOT NULL,
+    title VARCHAR   NOT NULL,
+    CONSTRAINT pk_Title PRIMARY KEY (
+        title_id
      )
 );
 
-ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employee" ("emp_no");
+ALTER TABLE Dept_emp ADD CONSTRAINT fk_Dept_emp_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employee (emp_no);
 
-ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no");
+ALTER TABLE Dept_emp ADD CONSTRAINT fk_Dept_emp_dept_no FOREIGN KEY(dept_no)
+REFERENCES Departments (dept_no);
 
-ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no");
+ALTER TABLE Dept_manager ADD CONSTRAINT fk_Dept_manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES Departments (dept_no);
 
-ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employee" ("emp_no");
+ALTER TABLE Dept_manager ADD CONSTRAINT fk_Dept_manager_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employee (emp_no);
 
-ALTER TABLE "Employee" ADD CONSTRAINT "fk_Employee_emp_title" FOREIGN KEY("emp_title")
-REFERENCES "Title" ("title_id");
+ALTER TABLE Employee ADD CONSTRAINT fk_Employee_emp_title FOREIGN KEY(emp_title)
+REFERENCES Title (title_id);
 
-ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employee" ("emp_no");
+ALTER TABLE Salaries ADD CONSTRAINT fk_Salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employee (emp_no);
 
